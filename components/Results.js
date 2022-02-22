84a76b4d9f1075cea13e7a674d5a26ca027fa33f
akhjol99 <ahjol_tauke@yahoo.com>
2022-02-22 15:54:31 +0700

    first-commit

---
+++ components/Results.js
@@ -0,0 +1,10 @@
+import Thumbnail from "./Thumbnail"
+export default function Results({results}) {
+  return (
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3">
+        {results.map(result => (
+            <Thumbnail  key={result.id} result={result}/>
+        ))}
+    </div>
+  )
+}
